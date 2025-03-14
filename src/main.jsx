d3543558566444f998fc5cf7d7c55a612b570557
shoeb siddiqui <shoebahmedsiddiqui2747@gmail.com>
2025-03-14 10:28:44 +0530

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import "../src/assets/styles/style.scss"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
