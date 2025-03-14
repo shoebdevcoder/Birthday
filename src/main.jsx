28a8262d5ad532fa7c6535a2d28b9a39faf3a4b5
shoeb siddiqui <shoebahmedsiddiqui2747@gmail.com>
2025-03-14 10:38:39 +0530

    Changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import { HashRouter as Router } from 'react-router-dom'
 import "../src/assets/styles/style.scss"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
   <StrictMode>
     <App />
   </StrictMode>
-  </BrowserRouter>
+  </HashRouter>
 )
